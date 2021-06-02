d8dd11cf0efbb4ea467ca04a1cc7e85716cca7e3
Ishika <ishikalohia4@gmail.com>
2021-06-02 11:33:41 +0530

    Added navmenu items and Login button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <BrowserRouter>    
       <Switch>
         <Route exact path="/">
